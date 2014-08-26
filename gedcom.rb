00ee0f76c8d5640b4d5a374d2438575bdabfe452
Timothy Nordloh <tnordloh@gmail.com>
2014-08-26 10:16:55 -0600

    further decoupled, implemented struct, etc

--- gedcom.rb
+++ gedcom.rb
@@ -1,32 +1,65 @@
 #!/usr/bin/env ruby
 require 'rexml/document'
 require_relative 'name'
+Line = Struct.new(:level,:name,:data)
+
 class Gedcom
-  def self.parse_stack stack
-    current_level= []
-    el =Gedcom.create_element stack.shift 
-    current_level << el
-    stack.each_with_index {|item,i|
-      myel = Gedcom.create_element item 
-      current_level[-1].add_element myel
-      my_peek = stack[i+1]
-      if my_peek != nil 
-        if my_peek[0] > item[0]
-          current_level.push myel
-        elsif my_peek[0] < item[0]
-          current_level.pop
-        end
+  def initialize root_node
+    @root_node = REXML::Element.new root_node
+    @stack = []
+    @curline = nil
+  end
+  def process_line line
+    curline = parse_line line
+    if is_level_0? curline
+      add_stack
+    else
+      @stack << curline
+    end
+  end
+  def parse_line line
+    level,name,data = line.split %r{\s+}, 3
+    @curline = Line.new(level.to_i,name,data)
+  end
+  def is_level_0? line
+    line.level == 0 && @stack.size != 0
+  end
+  def add_stack
+    @root_node.add parse_stack 
+    @stack = []
+  end
+  def set_level current_level, element, item1,item2 = nil
+    if item2 != nil
+      if item2.level > item1.level
+        current_level.push element 
+      elsif item2.level < item1.level
+        current_level.pop 
       end
+    end
+    current_level
+  end
+  def parse_stack 
+    current_level= []
+    el =create_element @stack.shift 
+    current_level << el
+    @stack.each_with_index {|item,i|
+      myel = create_element item 
+      current_level[-1].add_element myel
+      current_level= set_level current_level,myel,item,@stack[i+1]
     }
     el
   end
-  def self.create_element(element)
-    if element[1] == "NAME" && element[2] =~ /\//
-      Name.new(element[2]).name_xml
+  def create_element(element)
+    if element.name == "NAME" && element.data =~ /\//
+      Name.new(element.data).name_xml
     else
-      el = REXML::Element.new element[1]
-      el.text=element[2]
+      el = REXML::Element.new element.name
+      el.text=element.data
       el
     end
+  end
+  def print
+    REXML::Formatters::Pretty.new.write @root_node, $stdout
+    puts
   end
 end
--- runme.rb
+++ runme.rb
@@ -2,20 +2,22 @@
 require 'rexml/document'
 require_relative 'gedcom'
 #main code.  Create root element
-gedcom = REXML::Element.new "GEDCOM"
 #empty stack, used to push and pop the relevant xml elements
-mystack=[]
+gedcom = Gedcom.new("GEDCOM")
 File.open('royal.ged').each_line do |line|
   line.chomp!
   next if line.empty? 
   #if the current line is "0", and stack isn't empty, pass this section to the xml parser
   #second condition deals with the first run, where mystack hasn't been initialized yet'
-  if line[0] == "0" && mystack.size != 0
-    gedcom.add Gedcom.parse_stack mystack
-    mystack = []
-  end
-  number,name,data = line.split %r{\s+}, 3
-  mystack << [number.to_i,name,data]
+  gedcom.process_line line
+  #if line[0] == "0" && mystack.size != 0
+  #  gedcom.add Gedcom.parse_stack mystack
+  #  mystack = []
+  #end
+  #level,name,data = line.split %r{\s+}, 3
+  #Line = Struct.new(:level,:name,:data)
+  #mystack << Line.new(number.to_i,name,data)
 end
-REXML::Formatters::Pretty.new.write gedcom, $stdout
+gedcom.print
+
 puts
