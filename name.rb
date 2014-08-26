3b30af363688a20db3f53d9b652a751998a6f312
Timothy Nordloh <tnordloh@gmail.com>
2014-08-25 22:12:38 -0600

    fixed surname to deal properly with null surnames

--- name.rb
+++ name.rb
@@ -3,23 +3,21 @@ require 'rexml/document'
 class Name
   attr_reader :name_xml
   def initialize(name)
-    @name=name.gsub!(/\//," ")
+    @name=name.gsub(/\//," ")
     @givn,@surn=name.split(/\s*\//)
-    if @surn == nil
-      @surn = " "
-    else
+  end
+  def name_xml
+    @name_xml = REXML::Element.new "NAME"
+    @name_xml.text=@name
+    if @surn != nil
       @surn.delete! "/" 
-    end
-    def name_xml
-      @name_xml = REXML::Element.new "NAME"
-      @name_xml.text=@name
       myel2 = REXML::Element.new "SURN"
       myel2.text=@surn
-      myel3 = REXML::Element.new "GIVN"
-      myel3.text=@givn
       @name_xml.add_element myel2
-      @name_xml.add_element myel3
-      @name_xml
     end
+    myel3 = REXML::Element.new "GIVN"
+    myel3.text=@givn
+    @name_xml.add_element myel3
+    @name_xml
   end
 end
