a893969369297fad21215407e523ab2d18be7dd3
Timothy Nordloh <tnordloh@gmail.com>
2014-08-27 16:14:00 -0600

    massive cleanup on aisle four

--- runme.rb
+++ runme.rb
@@ -1,16 +1,12 @@
 #!/usr/bin/env ruby
-require 'rexml/document'
+
 require_relative 'xml'
 require_relative 'gedcom'
-#main code.  Create root element
-#empty stack, used to push and pop the relevant xml elements
-gedcom = Gedcom.new("GEDCOM")
-File.open('royal.ged').each_line do |line|
-  line.chomp!
-  next if line.empty? 
-  gedcom.process_line line
+
+parser=Gedcom.new(File.open('royal.ged'))
+writer = XmlWriter.new($stdout)
+parser.each do |tree|
+  writer.write(tree)
 end
-writer = XmlWriter.new(gedcom.root)
-writer.print
 
 puts
